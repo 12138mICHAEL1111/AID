eb5ed2d14e7fafec40449f96f322425deedbcaf5
cuiyeshuai <61742973+cuiyeshuai@users.noreply.github.com>
2021-03-12 00:44:13 +0800

    first attempt to add interaction

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/select', //the page this app will start at
       onGenerateRoute: onGenerateRoute
     );
