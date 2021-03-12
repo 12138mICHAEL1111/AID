a10e8fe5ed8c30ce0b118c8e4d98976b7008c9fa
cuiyeshuai <61742973+cuiyeshuai@users.noreply.github.com>
2021-03-12 13:21:56 +0800

    Implement Select Page functions

--- flutter_project/lib/pages/Selectcategoriespage.dart
+++ flutter_project/lib/pages/Selectcategoriespage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screen_adapter/flutter_screen_adapter.dart';
 import './SchedulaerDatepage.dart';
 import 'package:adobe_xd/page_link.dart';
 import './Practicepage2.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Selectcategoriespage extends StatefulWidget {
   Selectcategoriespage({Key key}) : super(key: key);
@@ -9,10 +11,16 @@ class Selectcategoriespage extends StatefulWidget {
   _SelectPageState createState() => _SelectPageState();
 }
 
+
 class _SelectPageState extends State<Selectcategoriespage> {
   //The list store all the option
-  var _list = List<int>.filled(10,0);
-
+  var _list = List<int>.filled(10, 0);
+  var _check = false;
+  //user id
+  ///////////////////////////////////////////////////////////////////////////////////
+  var _id = 24;
+  var _id1 = 23;
+  
   //Called when pressed, update the list
   void tap(int i) {
     if (_list[i] == 0) {
@@ -26,7 +34,26 @@ class _SelectPageState extends State<Selectcategoriespage> {
 
   //Check if exactly 6 are selected
   bool check() {
-    return _list.length == 6;
+    int length = 0;
+    for (int i = 0; i < _list.length; ++i) {
+      if (_list[i] == 1) ++length;
+    }
+    if(length == 6) {
+      _check = true;
+      if(_id%2 == 1) {
+        print(0);
+      } else {
+        for(int i = 1; i < 8; ++i) {
+          if(_list[i] == 0) {
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+            print("giao $i");
+          }
+        }
+      }
+    } else {
+      _check =  false;
+    }
+    return _check;
   }
 
   //construct options
@@ -35,6 +62,7 @@ class _SelectPageState extends State<Selectcategoriespage> {
         onTap: () {
           setState(() {
             tap(i);
+            check();
           });
         },
         child: Container(
@@ -50,13 +78,35 @@ class _SelectPageState extends State<Selectcategoriespage> {
               style: TextStyle(
                 fontFamily: 'ZiZhiQuXiMaiTi',
                 fontSize: 24,
-                color:
-                    (_list[i] == 1 ? Color(0xffffffff) : Color(0xfffda873)),
+                color: (_list[i] == 1 ? Color(0xffffffff) : Color(0xfffda873)),
               ),
               textAlign: TextAlign.center,
             ),
           ),
         ));
+  }
+  _showToast() {
+    Fluttertoast.showToast(
+        msg: "Please select 6 options",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+  //used to get the question number
+  int result() {
+    if(_id%2 == 1) {
+      return 0;
+    } else {
+      for(int i = 1; i < 8; ++i) {
+        if(_list[i] == 0) {
+          return i;
+        }
+      }
+    }
   }
 
   @override
@@ -106,166 +156,100 @@ class _SelectPageState extends State<Selectcategoriespage> {
           ),
           //!!!!!!!!!!!!
           Transform.translate(
-            offset: Offset(41.0, 344.0),
-            child: cons(1, "Social")
-          ),
+              offset: Offset(41.0, 344.0), child: cons(1, "Social")),
           Transform.translate(
-            offset: Offset(232.0, 344.0),
-            child: cons(2, "Academic")
-          ),
+              offset: Offset(232.0, 344.0), child: cons(2, "Academic")),
           Transform.translate(
-            offset: Offset(41.0, 417.0),
-            child: cons(3, "Hobbies")
-          ),
+              offset: Offset(41.0, 417.0), child: cons(3, "Hobbies")),
           Transform.translate(
-            offset: Offset(41.0, 492.0),
-            child: cons(4, "Family")
-          ),
+              offset: Offset(41.0, 492.0), child: cons(4, "Family")),
           Transform.translate(
-            offset: Offset(232.0, 417.0),
-            child: cons(5, "Work")
-          ),
+              offset: Offset(232.0, 417.0), child: cons(5, "Work")),
           Transform.translate(
-            offset: Offset(222.0, 492.0),
-            child: cons(6, "Relationships")
-          ),
+              offset: Offset(222.0, 492.0), child: cons(6, "Relationships")),
           Transform.translate(
-            offset: Offset(106.0, 565.0),
-            child: cons(7, "Mood and Health")
-          ),
-          // Transform.translate(
-          //   offset: Offset(75.0, 356.0),
-          //   child: SizedBox(
-          //     width: 78.0,
-          //     child: Text(
-          //       'Social',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffda873),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(244.0, 356.0),
-          //   child: SizedBox(
-          //     width: 122.0,
-          //     child: Text(
-          //       'Academic',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffca772),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(63.0, 427.0),
-          //   child: SizedBox(
-          //     width: 102.0,
-          //     child: Text(
-          //       'Hobbies',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffda672),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(262.0, 427.0),
-          //   child: SizedBox(
-          //     width: 86.0,
-          //     child: Text(
-          //       'Family',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffca772),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(72.0, 504.0),
-          //   child: SizedBox(
-          //     width: 74.0,
-          //     child: Text(
-          //       'Work',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffca772),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(221.4, 504.0),
-          //   child: SizedBox(
-          //     width: 167.0,
-          //     child: Text(
-          //       'Relationships',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffda873),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
-          // Transform.translate(
-          //   offset: Offset(104.5, 577.0),
-          //   child: SizedBox(
-          //     width: 209.0,
-          //     child: Text(
-          //       'Mood and Health',
-          //       style: TextStyle(
-          //         fontFamily: 'ZiZhiQuXiMaiTi',
-          //         fontSize: 24,
-          //         color: const Color(0xfffda873),
-          //       ),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //   ),
-          // ),
+              offset: Offset(106.0, 565.0), child: cons(7, "Mood and Health")),
           Transform.translate(
             offset: Offset(129.0, 644.0),
-            child: PageLink(
-              links: [
-                PageLinkInfo(
-                  transition: LinkTransition.PushLeft,
-                  ease: Curves.easeOut,
-                  duration: 1.0,
-                  pageBuilder: () => SchedulaerDatepage(),
-                ),
-              ],
-              child: Container(
-                width: 171.0,
-                height: 62.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(23.0),
-                  color: const Color(0xffffffff),
-                  boxShadow: [
-                    BoxShadow(
-                      color: const Color(0x29000000),
-                      offset: Offset(0, 13),
-                      blurRadius: 6,
-                    ),
-                  ],
+            child: InkWell(
+              onTap: () {       
+                check();
+                print("tapped");
+                if(check()) {
+                  print(result());
+                } else {
+                  _showToast();
+                } 
+              },
+
+              // child: PageLink(
+              //   links: [
+              //     check()
+              //     ?PageLinkInfo(
+              //       transition: LinkTransition.PushLeft,
+              //       ease: Curves.easeOut,
+              //       duration: 1.0,
+              //       pageBuilder: () => SchedulaerDatepage(),
+              //     )
+              //     :null
+              //   ],
+                child: Container(
+                  width: 171.0,
+                  height: 62.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(23.0),
+                    color: const Color(0xffffffff),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color(0x29000000),
+                        offset: Offset(0, 13),
+                        blurRadius: 6,
+                      ),
+                    ],
+                  ),
+                  child: _check?PageLink(
+                    links: [
+                      PageLinkInfo(
+                        transition: LinkTransition.PushLeft,
+                        ease: Curves.easeOut,
+                        duration: 1.0,
+                        pageBuilder: () => SchedulaerDatepage(),
+                      )
+                    ],
+                  )
+                  :null
                 ),
               ),
             ),
-          ),
+
+          // Transform.translate(
+          //   offset: Offset(129.0, 644.0),
+          //   child: PageLink(
+          //     links: [
+          //       PageLinkInfo(
+          //         transition: LinkTransition.PushLeft,
+          //         ease: Curves.easeOut,
+          //         duration: 1.0,
+          //         pageBuilder: () => SchedulaerDatepage(),
+          //       ),
+          //     ],
+          //     child: Container(
+          //       width: 171.0,
+          //       height: 62.0,
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(23.0),
+          //         color: const Color(0xffffffff),
+          //         boxShadow: [
+          //           BoxShadow(
+          //             color: const Color(0x29000000),
+          //             offset: Offset(0, 13),
+          //             blurRadius: 6,
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
           Transform.translate(
             offset: Offset(158.8, 649.0),
             child: SizedBox(
@@ -285,27 +269,27 @@ class _SelectPageState extends State<Selectcategoriespage> {
             offset: Offset(20.0, 42.0),
             child:
                 // Adobe XD layer: 'jiantou' (shape)
-                PageLink(
-              links: [
-                PageLinkInfo(
-                  transition: LinkTransition.PushRight,
-                  ease: Curves.easeIn,
-                  duration: 1.0,
-                  pageBuilder: () => Practicepage2(),
-                ),
-              ],
-              child: Container(
-                width: 50.0,
-                height: 50.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: const AssetImage('assets/images/goback.png'),
-                    fit: BoxFit.fill,
+              PageLink(
+                links: [
+                  PageLinkInfo(
+                    transition: LinkTransition.PushRight,
+                    ease: Curves.easeIn,
+                    duration: 1.0,
+                    pageBuilder: () => Practicepage2(),
+                  )
+                ],
+                child: Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: const AssetImage('assets/images/goback.png'),
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
         ],
       ),
     );
