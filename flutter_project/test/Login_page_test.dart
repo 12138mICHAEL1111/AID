59428412eea13f85a6055d272522ece53338744d
yumengsihai <15175333629@163.com>
2021-04-03 10:52:02 +0800

    "dio error"

--- flutter_project/test/Login_page_test.dart
+++ flutter_project/test/Login_page_test.dart
@@ -1,14 +1,32 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_project/config/Config.dart';
 import 'package:flutter_project/pages/Loginpage.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
 
 void main(){
+  final dio = Dio();
+  final dioAdapter = DioAdapter();
+  dio.httpClientAdapter = dioAdapter;
+  var path = '${Config.domain}/rest/users/login';
+
   Widget createWidgetForTesting({Widget child}) {
     return MaterialApp(
       home: child,
     );
   }
+
+  test('test Dio', () async{
+    dioAdapter
+        .onPost(path)
+        .reply(200, {'message': 'success'});
+
+    final onPostResponse = await dio.post(path);
+    print(onPostResponse.data); // {message: Successfully mocked POST!}
+  });
+
   testWidgets('Login page content test', (WidgetTester tester) async {
     await tester.pumpWidget(createWidgetForTesting(child: new Loginpage()));
     await tester.pumpAndSettle();
