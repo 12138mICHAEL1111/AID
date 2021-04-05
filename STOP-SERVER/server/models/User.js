1d73dcc602ac8a98b5bbaf6b8350a74503c9efa8
12138mICHAEL1111 <1158969792@qq.com>
2021-04-06 01:07:16 +0800

    fix some server bugs

--- STOP-SERVER/server/models/User.js
+++ STOP-SERVER/server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const brcypt = require('brcypt')
+const bcrypt = require('bcrypt')
 const schema = new mongoose.Schema({
     email: { type: String },
     userid: {
@@ -32,7 +32,7 @@ const schema = new mongoose.Schema({
     password: {
         type: String,
         set(val) {
-            return brcypt.hashSync(val, 10)
+            return bcrypt.hashSync(val, 10)
     }
 }
 })
