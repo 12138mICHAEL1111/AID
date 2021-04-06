7204b13023e1f6168aef336d92f5f8e2c078cc08
12138mICHAEL1111 <1158969792@qq.com>
2021-04-07 01:17:58 +0800

    change the api address

--- flutter_project/lib/config/Config.dart
+++ flutter_project/lib/config/Config.dart
@@ -1,5 +1,5 @@
 class Config {
-  //static String domain = "http://192.168.3.34:3000/stop/api";
+  static String domain = "http://192.168.3.34:3000/stop/api";
   //网络地址
-  static String domain = "https://stop-variants.herokuapp.com/stop/api";
+  //static String domain = "https://stop-variants.herokuapp.com/stop/api";
 }
